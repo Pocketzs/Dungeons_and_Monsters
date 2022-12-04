516c09180ae814d3c869ff1fb44058a19ed7846e
Nigel Aung-Myint <nigel.aungmyint@gmail.com>
2022-12-03 19:10:19 -0600

    add dungeon index created at test

--- spec/features/dungeons/index_spec.rb
+++ spec/features/dungeons/index_spec.rb
@@ -45,12 +45,20 @@ RSpec.describe "Dungeons Index", type: :feature do
 
       it ' I see that records are ordered by most recently created first' do
         visit "/dungeons"
+
         expect(@dungeon_4.name).to appear_before(@dungeon_3.name)
         expect(@dungeon_3.name).to appear_before(@dungeon_2.name)
         expect(@dungeon_2.name).to appear_before(@dungeon_1.name)
       end
 
-      it 'Next to each of the records I see when it was created'
+      it 'Next to each of the records I see when it was created' do
+        visit "/dungeons"
+
+        expect(page).to have_content("Created at: #{@dungeon_1.created_at}")
+        expect(page).to have_content("Created at: #{@dungeon_2.created_at}")
+        expect(page).to have_content("Created at: #{@dungeon_3.created_at}")
+        expect(page).to have_content("Created at: #{@dungeon_4.created_at}")
+      end
     end
   end
 end
